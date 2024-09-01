582a07f8caf09e081269cbc45c939d09a2492d0e
hendrikTpl <hendrik.gian@gmail.com>
2024-09-01 17:13:38 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ module.exports = {
   content: [
     './layouts/**/*.html',
     './content/**/*.md',
+    './themes/**/*.{html,js}', // If you use themes
+    './partials/**/*.html',
+    './static/**/*.html',
+    './src/**/*.{html,js}', // If you have a src directory
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
